ae1e02104b041cda1de761a233a4cfd081fdb1f1
Kisara <214858752@qq.com>
2025-02-15 18:19:45 +0800

    fix:Removed unnecessary code and fixed route nesting issues in the Login folder

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import {Text, View} from 'react-native';
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import Otherindex from './src_other/Otherindex';
+import Login from './src_Login/Login';
 export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Otherindex />
+        <Login />
       </NavigationContainer>
     );
   }
