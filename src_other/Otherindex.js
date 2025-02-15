21667afb4091207e4c7da1cf1ee791c39560e884
Kisara <214858752@qq.com>
2025-02-15 17:49:19 +0800

    feat:added the rounting function and classification

---
+++ src_other/Otherindex.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import DirectionPad from '../components/DirectionPad';
+import FunctionKeys from '../components/FunctionKeys';
+import SettingsButton from '../components/SettingButton';
+import StatusView from '../components/StatusView';
+import SystemNavigationBar from 'react-native-system-navigation-bar';
+
+const Otherindex = () => {
+  SystemNavigationBar.fullScreen(true); // 隐藏系统导航栏
+
+  const [isStatusViewVisible, setIsStatusViewVisible] = useState(false); // 控制 StatusView 的显示状态
+
+  const handleDirectionPress = direction => {
+    console.log(`Pressed ${direction} button`);
+    // 你可以在这里添加处理方向键按下的逻辑
+  };
+
+  // 设置按钮的点击事件
+  const handleSettingsPress = () => {
+    console.log('Settings button pressed');
+    setIsStatusViewVisible(!isStatusViewVisible); // 切换 StatusView 的显示状态
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <DirectionPad onPress={handleDirectionPress} />
+        <FunctionKeys />
+        {/* 根据状态变量控制 StatusView 的显示 */}
+        {isStatusViewVisible && <StatusView />}
+        <SettingsButton onPress={handleSettingsPress} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
+    alignItems: 'center',
+  },
+});
+
+export default Otherindex;
